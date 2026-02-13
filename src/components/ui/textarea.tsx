1227ca91b99fa097b3adc9d458044269c70d7761
zyad007 <zyad.sallem.007@gmail.com>
2026-02-13 18:52:51 +0200

    feat q/a section

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "w-full min-w-0 border border-border-default px-6 py-4 rounded-2xl shadow-[0px_2px_4px_0px_#0000000A] text-primary-copy text-lg focus:outline-none focus:ring-2 focus:ring-primary transition-shadow duration-200",
+        "w-full min-w-0 border border-border-default px-6 py-4 rounded-2xl shadow-[0px_2px_4px_0px_#0000000A] text-primary-copy text-base focus:outline-none focus:ring-2 focus:ring-primary transition-shadow duration-200",
         "placeholder:text-muted-foreground flex field-sizing-content min-h-16 disabled:cursor-not-allowed disabled:opacity-50",
         "aria-invalid:ring-destructive/20 aria-invalid:border-destructive placeholder:font-medium",
         className
